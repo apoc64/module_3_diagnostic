756337b359b7f7da6bceddff8effe0f7662594c1
apoc64 <sschwedt@gmail.com>
2018-07-11 09:23:49 -0600

    search goes to search controller and view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    
   end
 end
+
+# ENV["api_key"]
+
+# https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,22 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+
+  it 'shows search results' do
+    visit '/'
+
+    fill_in 'q', with: '80203'
+    click_on 'Locate'
+
+    expect(current_path).to eq('/search')
+    expect(page).to have_content("Results")
+  end
 end
+# ```As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times```
